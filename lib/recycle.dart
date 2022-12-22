d9f72e57cf852b3d9bf7aff1c940bba11e5176b5
thejzain <thejzainkp@gmail.com>
2022-12-22 19:04:20 +0530

    first

---
+++ lib/recycle.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Recycle extends StatelessWidget {
+  const Recycle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Main();
+  }
+}
+
+class Main extends StatefulWidget {
+  Main({Key? key}) : super(key: key);
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
