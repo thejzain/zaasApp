d78447a17640e204cabbf8151c92c2c7c6d36469
Agastus Joyson <agastusjoyson@gmail.com>
2022-12-23 00:13:16 +0530

    Login page

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.green),
       home: Scaffold(
-        body: Recycle(),
+        body: LoginPage(),
       ),
     );
   }
